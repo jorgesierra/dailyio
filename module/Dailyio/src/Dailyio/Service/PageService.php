003ff80bb3b329d71e3eb9f1abd857d810ea758e
jorgesierra <georgesierra@gmail.com>
2013-09-09 15:28:07 -0300

    Fix findLastActivityDate

--- module/Dailyio/src/Dailyio/Service/PageService.php
+++ module/Dailyio/src/Dailyio/Service/PageService.php
@@ -83,7 +83,7 @@ class PageService implements ServiceLocatorAwareInterface
           FROM pages p, pages_data pd
 	      WHERE p.id = pd.page_id 
 	           AND p.id = '".$Page->getId()."' 
-	           AND pd.page_date <> '".date('Y-m-d')."'".$fromDateSql."'
+	           AND pd.page_date <> '".date('Y-m-d')."'".$fromDateSql." 
 		  ORDER BY pd.id DESC LIMIT 1;   
         ";
 	    
