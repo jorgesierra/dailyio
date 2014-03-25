2f0ee8ce29b0c3f6c6bf88f5209e72b559781cf1
jorgesierra <georgesierra@gmail.com>
2014-03-25 11:05:54 -0300

    time tracker

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -15,3 +15,83 @@ $(function() {
 			window.location = "/"+pageUrl+"/"+$('#dailyio-datepicker').attr('data-page-hash')+"/"+selDateStr;
     	});
 });
+
+function timeToMs(hours, minutes) {
+	var ms = 0;
+	var oneMin = 60000;
+
+	hours = parseInt(hours);
+	minutes = parseInt(minutes);
+
+	if(minutes == 30) {
+		ms = 30 * oneMin;
+	}
+
+	ms = ms + (hours * oneMin * 60);
+
+	return ms;
+}
+
+function msToTime(ms) {
+	var x = ms / 1000;
+	var seconds = Math.floor(x % 60);
+	x /= 60;
+	var minutes = Math.floor(x % 60);
+	x /= 60;
+	var hours = Math.floor(x % 24);
+	x /= 24;
+	var days = Math.floor(x);
+
+	if(seconds < 10) {
+		seconds = "0"+seconds;
+	}
+	var timeStr = "00:00:"+seconds;
+
+	if(minutes >= 0) {
+		if(minutes < 10) {
+			minutes = "0"+minutes;
+		}
+		timeStr = "00:"+minutes;
+	}
+
+	if(hours >= 0) {
+		if(hours < 10) {
+			hours = "0"+hours;
+		}
+		timeStr = hours+":"+minutes;
+	}
+
+	if(days > 0) {
+		var dayStr = "day";
+		if(days > 1) {
+			dayStr = "days";
+		}
+		timeStr = days+" days, "+hours+":"+minutes;
+	}
+	return timeStr;
+}
+
+function createTimeSelect() {
+	console.log("pasa");
+	var minArray = ['00','30'];
+	var $select = $('<select id="time-select" class="time-select"><option value="">Select</option></select>');
+
+	for(i = 0; i < 24; i++) {
+		$.each(minArray, function(index, value) {
+			if(i == 0 && value == "00") {
+			} else {
+				var timeStr = i;
+				if(i < 10) {
+					timeStr = "0"+i;
+				}
+				timeStr+=":"+value;
+
+				$select.append("<option value='"+timeToMs(i, value)+"'>"+timeStr+"</option>");
+			}
+		});
+	}
+
+	$span = $("<span class='time-select-container'><i class='fa fa-times-circle'></i></span>");
+	$span.prepend($select);
+	$('body').append($span);
+}
